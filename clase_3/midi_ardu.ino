8fdd9bb5ba86ee36b433bbfbcc01a8f263e2910f
Bruno Perelli Soto <bperelli@uchile.cl>
2025-04-15 18:09:40 -0400

    Create midi_ardu.ino

---
+++ clase_3/midi_ardu.ino
@@ -0,0 +1,42 @@
+ #include "MIDIUSB.h"
+
+// First parameter is the event type (0x09 = note on, 0x08 = note off).
+// Second parameter is note-on/note-off, combined with the channel.
+// Channel can be anything between 0-15. Typically reported to the user as 1-16.
+// Third parameter is the note number (48 = middle C).
+// Fourth parameter is the velocity (64 = normal, 127 = fastest).
+
+void noteOn(byte channel, byte pitch, byte velocity) {
+  midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
+  MidiUSB.sendMIDI(noteOn);
+}
+
+void noteOff(byte channel, byte pitch, byte velocity) {
+  midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, velocity};
+  MidiUSB.sendMIDI(noteOff);
+}
+
+void setup() {
+  Serial.begin(115200);
+}
+
+// First parameter is the event type (0x0B = control change).
+// Second parameter is the event type, combined with the channel.
+// Third parameter is the control number number (0-119).
+// Fourth parameter is the control value (0-127).
+
+void controlChange(byte channel, byte control, byte value) {
+  midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
+  MidiUSB.sendMIDI(event);
+}
+
+void loop() {
+  int ADC_SHARP_A = analogRead(A0);
+  //map para los valores del sensor a midi
+  int note1 = map(ADC_SHARP_A, 0, 1023, 0, 127);
+  SerialUSB.println("Sending note on");
+  noteOn(1, note1, 64);   // Channel 0, middle C, normal velocity
+  MidiUSB.flush();
+  delay(64);
+  SerialUSB.println("Sending note off");
+}
