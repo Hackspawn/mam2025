b8bd45620fa9ecacbe4e2bbf8da74e20f8de64a7
Bruno Perelli Soto <bperelli@uchile.cl>
2025-06-25 17:44:53 -0400

    Update receptor.ino

--- clase_11/receptor.ino
+++ clase_11/receptor.ino
@@ -1,9 +1,9 @@
 #include <WiFi.h>
 #include <WebSocketsServer.h>
-#include <Servo.h>
+#include <ESP32Servo.h>
 
-const char* ssid = "TU_SSID"; //nombre Red Wifi
-const char* password = "TU_PASS"; //clave Red Wifi
+const char* ssid = "TU_SSID";
+const char* password = "TU_PASS";
 
 Servo myServo;
 WebSocketsServer webSocket(81);
@@ -12,7 +12,7 @@ void handleUID(String uid) {
   int angle = 0;
   if (uid == "a1b2c3") angle = 30;
   else if (uid == "d4e5f6") angle = 90;
-  else angle = 0; // default
+  else angle = 0;
   myServo.write(angle);
 }
 
@@ -25,10 +25,20 @@ void onWebSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t leng
 
 void setup() {
   Serial.begin(115200);
+  myServo.setPeriodHertz(50);  // 👈 necesario para ESP32
   myServo.attach(13);
 
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) delay(500);
+  Serial.print("Conectando a WiFi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.println("Conectado a WiFi.");
+  Serial.print("Dirección IP: ");
+  Serial.println(WiFi.localIP());
 
   webSocket.begin();
   webSocket.onEvent(onWebSocketEvent);
